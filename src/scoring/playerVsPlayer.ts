7808eba1a38e12346197316aef91ecb009535dfe
Morten Jensen <mje@monthio.com>
2024-07-04 22:36:32 +0200

    Fixes runModels

--- src/scoring/playerVsPlayer.ts
+++ src/scoring/playerVsPlayer.ts
@@ -14,43 +14,26 @@ interface PvPScores {
   trueSkill: TrueSkillScore;
 }
 
+// Maybe have a picker of values?
 const scoringModels: [score: string, model: PlayerVsPlayer<any>][] = [
   ["elo", eloService],
   ["glicko2", glicko2Service],
   ["trueSkill", trueSkillService],
 ];
+
 const runModels =
   <T extends keyof PlayerVsPlayer<PvPScores>>(call: T) =>
   (...args: Parameters<PlayerVsPlayer<PvPScores>[T]>) =>
     scoringModels.reduce((acc, [score, model]) => {
-      const result = model[call].apply(null, args as any);
+      const [winner = {}, looser = {}] = args as any;
       return {
         ...acc,
-        [score]: result,
+        [score]: model[call](winner[score], looser[score]),
       };
     }, {} as ReturnType<PlayerVsPlayer<PvPScores>[T]>);
 
 class PlayerVsPlayerService implements PlayerVsPlayer<PvPScores> {
   public readonly match = (winner: PvPScores, looser: PvPScores) => {
-    const elo = eloService.match(winner.elo, looser.elo);
-    const glicko2 = glicko2Service.match(winner.glicko2, looser.glicko2);
-    const trueSkill = trueSkillService.match(
-      winner.trueSkill,
-      looser.trueSkill
-    );
-    return {
-      winner: {
-        elo: elo.winner,
-        glicko2: glicko2.winner,
-        trueSkill: trueSkill.winner,
-      },
-      looser: {
-        elo: elo.looser,
-        glicko2: glicko2.looser,
-        trueSkill: trueSkill.looser,
-      },
-    };
-
     return runModels("match")(winner, looser);
   };
 
